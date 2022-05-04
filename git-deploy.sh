8c57b24db5267fcb8dab729a657d6e2960b5091b
sofiasusannna <sofia.raejarvi@student.hamk.fi>
2022-05-04 09:38:44 +0000

    testi123

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-gh auth login
+git add .
 
-if [ $? -ne 0 ];
-then
-    echo "You are not in the proper directory"
-else
-    read -p "Do you want to commit and push your changes (y/n)? " ANSWER
-    if [ $ANSWER == 'y' ];
-    then
-	echo "You're about to push your changes"
-	git add .
-	read -p "What's the message you desire to type? " MESSAGE
-	git commit -m "$MESSAGE"
-	read -p "What's the name of the destination branch? " DESTINATION_BRANCH
-	git push -u origin $DESTINATION_BRANCH
-    else
-	echo "Hope to see you again!"
-    fi
+read -p "Commit message: " COMMIT
+git commit -m "$COMMIT"
+
+read -p "Branch: " BRANCH
+
+git push -f origin $BRANCH
+
 fi
